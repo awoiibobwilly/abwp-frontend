6cc25b910f468f3873a27ca3a2731591604cadd7
awoiibobwilly <awoiibobwilly@gmail.com>
2026-06-15 01:45:19 +0300

    feat: implement highlights section

---
+++ src/data/highlightData.js
@@ -0,0 +1,25 @@
+export const highlightData = [
+    {
+      number: 5,
+      suffix: "+",
+      title: "Domains",
+    },
+  
+    {
+      number: 20,
+      suffix: "+",
+      title: "Projects",
+    },
+  
+    {
+      number: 10,
+      suffix: "+",
+      title: "Technologies",
+    },
+  
+    {
+      number: 100,
+      suffix: "%",
+      title: "Commitment",
+    },
+  ];
