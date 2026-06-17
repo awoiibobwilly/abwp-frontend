44bf72a9be325b0757369ced7d29f99e994a664b
awoiibobwilly <awoiibobwilly@gmail.com>
2026-06-17 10:27:05 +0300

    about: added Certifications Section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import AboutHero from "../components/about/AboutHero";
 import WhoIAm from "../components/about/WhoIAm";
 import ProfessionalSummary from "../components/about/ProfessionalSummary";
 import Education from "../components/about/Education";
+import Certifications from "../components/about/Certifications";
 
 function About() {
   return (
@@ -14,7 +15,9 @@ function About() {
 
       <Education />
 
-      
+      <Certifications />
+
+
     </>
   );
 }
