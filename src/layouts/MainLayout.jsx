816380611c1a5f95ba0d046673d456b2edee792c
awoiibobwilly <awoiibobwilly@gmail.com>
2026-06-20 02:44:19 +0300

    shared: added page transistion

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,18 +3,22 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
 import ScrollProgress from "../components/shared/ScrollProgress";
+import PageTransition from "../components/shared/PageTransition";
 
 function MainLayout() {
   return (
     <>
-      
-      <Navbar />
-
       <ScrollProgress />
+
+      <Navbar />
 
       <main className="main-content">
 
-        <Outlet />
+      <PageTransition>
+
+      <Outlet />
+
+      </PageTransition>
 
       </main>
 
