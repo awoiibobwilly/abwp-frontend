6ad6d2b0bf772f46f709627a72ebaaef727767b1
awoiibobwilly <awoiibobwilly@gmail.com>
2026-06-20 02:14:16 +0300

    shared: added scrollprogress

---
+++ src/components/shared/ScrollProgress.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+import "../../styles/shared/scrollProgress.css";
+
+function ScrollProgress() {
+
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+
+      const totalHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const progress =
+        (window.scrollY / totalHeight) * 100;
+
+      setScrollProgress(progress);
+
+    };
+
+    window.addEventListener(
+      "scroll",
+      handleScroll
+    );
+
+    return () =>
+      window.removeEventListener(
+        "scroll",
+        handleScroll
+      );
+
+  }, []);
+
+  return (
+
+    <div
+      className="scroll-progress"
+      style={{
+        width: `${scrollProgress}%`,
+      }}
+    />
+
+  );
+
+}
+
+export default ScrollProgress;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
+import ScrollProgress from "../components/shared/ScrollProgress";
 
 function MainLayout() {
   return (
     <>
-
+      
       <Navbar />
+
+      <ScrollProgress />
 
       <main className="main-content">
 
