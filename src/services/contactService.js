aa2a1449504118fe9202850d45efb88c113931a5
awoiibobwilly <awoiibobwilly@gmail.com>
2026-06-22 14:17:56 +0300

    general: handled corrupted files

---
+++ src/services/contactService.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const API_URL =
+
+`${import.meta.env.VITE_API_BASE_URL}/contact/`;
+
+export const sendContactMessage = async (
+
+  data
+
+) => {
+
+  try {
+
+    const response = await axios.post(
+
+      API_URL,
+
+      data
+
+    );
+
+    return response.data;
+
+  }
+
+  catch (error) {
+
+    throw (
+
+      error.response?.data ||
+
+      error.message
+
+    );
+
+  }
+
+};
